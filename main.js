50bda3dd53786cb39f72086b736508d5e5fe47db
Eduardo <eduardonaves02@outlook.com>
2022-05-30 17:01:20 -0300

    Atualização reponsividade

--- main.js
+++ main.js
@@ -2,3 +2,26 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector("header")
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+const btnMobile = document.getElementById('btn__mobile');
+
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefalult()
+    const nav = document.querySelector(".nav");
+    nav.classList.toggle('active')
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active)
+    if(active){
+        
+     event.currentTarget.setAttribute('aria-label', 'fechar menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'abrir menu')
+    }
+
+}
+
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+
